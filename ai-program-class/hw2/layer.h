11fa6d612dc92aeaef4e843db1d25a991fb5cfce
a-little-hoof <wyf181030@gmail.com>
2024-10-22 16:27:35 +0800

    layers init

---
+++ ai-program-class/hw2/layer.h
@@ -0,0 +1,17 @@
+
+// input: input X and fc parameters W
+// output: output Y
+// X's shape is [N, C_in]
+// Y's shape is [N, C_out]
+// N, C_in, C_out can be defined by users 
+void fc_forward(){
+
+}
+
+// input: partial L / partial y
+// output: parital L / partial W and partial L / partial X
+
+void fc_backward(){
+
+}
+
